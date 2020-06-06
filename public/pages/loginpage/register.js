fd33f7c6262023ab81798e62de6a5ae2e60def5e
jessicamelise <jessica.melise@hotmail.com>
2020-06-05 21:10:19 -0300

    adicionando css na pagina de registro mobile first

--- public/pages/loginpage/register.js
+++ public/pages/loginpage/register.js
@@ -1,22 +1,26 @@
 export const register = () => {
   const container = document.createElement("div");
+  container.classList.add("display-column");
 
   container.innerHTML = `
-    <a href="/#">
-    <button id="back-button">Voltar</button>
-    </a>
-        <label> Digite seu nome Completo </label>
-        <input id="name-input-register" type="text">
-        <label> Digite seu e-mail </label>
-        <input id="email-input-register" type="email">
-        <label> Digite sua data de nascimento </label>
-        <input id="date-input-register" type="date">
-        <label> Digite sua senha nova </label>
-        <input id="password-input-register" type="password">
-        <a href="/#posts" >
-        <button id="sign-in-button">Registrar-se</button>
-        </a>
-
-    `;
+    <img class="img" src="img/logo.png" alt="logo">
+    <img class="img" src="img/name.png" alt="name">
+    <label class="letter-color">Digite seu nome Completo:</label>
+    <input class="style-input" id="name-input-register" type="text">
+    <label class="letter-color">Digite seu e-mail:</label>
+    <input class="style-input" id="email-input-register" type="email">
+    <label class="letter-color">Digite sua data de nascimento:</label>
+    <input class="style-input" id="date-input-register" type="date">
+    <label class="letter-color">Digite uma senha:</label>
+    <input class="style-input" id="password-input-register" type="password">
+    <div class="register-display-buttons">
+      <a class="width-button-login" href="/#">
+        <button class="button-login" id="back-button">Voltar</button>
+      </a>
+      <a class="width-button-login" href="/#posts" >
+        <button class="button-login" id="sign-in-button">Registrar-se</button>
+      </a>
+    </div>
+  `;
   return container;
 };
