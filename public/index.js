2650fbdb0e321b0656aace794acb898e4e4dc8e6
Caroline Pinheiro <ommasshi@gmail.com>
2020-06-10 10:15:39 -0300

    Função de login com o google, enviada ao data.js

--- public/index.js
+++ public/index.js
@@ -42,7 +42,7 @@ function init() {
 
       const googleAuth = document.querySelector("#google");
 
-      googleAuth.addEventListener("click", googleLogin);
+      googleAuth.addEventListener("click", elements.googleLogin);
     }
   });
 }
@@ -110,42 +110,3 @@ function readPosts() {
   });
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-function googleLogin() {
-  let provider = new firebase.auth.GoogleAuthProvider();
-
-  firebase
-    .auth()
-    .signInWithPopup(provider)
-    .then(function (result) {
-      // This gives you a Google Access Token. You can use it to access the Google API.
-      let token = result.credential.accessToken;
-      // The signed-in user info.
-      let user = result.user;
-      // ...
-    })
-    .catch(function (error) {
-      // Handle Errors here.
-      let errorCode = error.code;
-      let errorMessage = error.message;
-      // The email of the user's account used.
-      let email = error.email;
-      // The firebase.auth.AuthCredential type that was used.
-      let credential = error.credential;
-      // ...
-    });
-}
