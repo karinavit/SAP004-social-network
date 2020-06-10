531ea6a281dd27739f5a445700500179e075d2e3
Caroline Pinheiro <ommasshi@gmail.com>
2020-06-10 15:32:47 -0300

    Correção do export da funçaõ de login com o google

--- public/data.js
+++ public/data.js
@@ -28,30 +28,6 @@ export const firebaseActions = {
         alert(error.message);
       });
   },
-  googleLogin() {
-    let provider = new firebase.auth.GoogleAuthProvider();
-
-    firebase
-      .auth()
-      .signInWithPopup(provider)
-      .then(function (result) {
-        // This gives you a Google Access Token. You can use it to access the Google API.
-        let token = result.credential.accessToken;
-        // The signed-in user info.
-        let user = result.user;
-        // ...
-      })
-      .catch(function (error) {
-        // Handle Errors here.
-        let errorCode = error.code;
-        let errorMessage = error.message;
-        // The email of the user's account used.
-        let email = error.email;
-        // The firebase.auth.AuthCredential type that was used.
-        let credential = error.credential;
-        // ...
-      });
-  },
   postData(post) {
     const postCollection = firebase.firestore().collection("posts");
 
@@ -76,6 +52,31 @@ export function readPosts() {
       }
     });
   });
+}
+
+export function googleLogin() {
+  let provider = new firebase.auth.GoogleAuthProvider();
+
+  firebase
+    .auth()
+    .signInWithPopup(provider)
+    .then(function (result) {
+      // This gives you a Google Access Token. You can use it to access the Google API.
+      let token = result.credential.accessToken;
+      // The signed-in user info.
+      let user = result.user;
+      // ...
+    })
+    .catch(function (error) {
+      // Handle Errors here.
+      let errorCode = error.code;
+      let errorMessage = error.message;
+      // The email of the user's account used.
+      let email = error.email;
+      // The firebase.auth.AuthCredential type that was used.
+      let credential = error.credential;
+      // ...
+    });
 }
 
 
