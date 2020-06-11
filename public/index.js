27281a8676061aa054f2e75056381dd79272f595
jessicamelise <jessica.melise@hotmail.com>
2020-06-11 02:15:36 -0300

    refatoração da função init, sepando dom de funções puras

--- public/index.js
+++ public/index.js
@@ -1,49 +1,26 @@
-import { routes } from "./routes.js";
+import { loggoutMenuEvent, pagePost, loginEvent, registerEvent, loginEventGoogle } from "./main.js";
+import { formLogin } from "./pages/login/login.js";
 import { signIn } from "./pages/posts/posts.js";
-import {registerDOM , postDOM} from "./main.js"
-import {readPosts, googleLogin} from "./data.js"
+import { register } from "./pages/register/register.js";
+
+export const routes = {
+  home: formLogin(),
+  posts: signIn(name),
+  register: register(),
+};
+
 const container = document.querySelector("#root");
 
 function init() {
   firebase.auth().onAuthStateChanged((user) => {
     if (user) {
-      container.innerHTML = "";
-      container.appendChild(signIn(firebase.auth().currentUser.displayName));
-      const loggoutButton = document.querySelector("#loggout");
-      const menuBar = document.querySelector("#bar-menu");
-      menuBar.addEventListener("click", () => {
-        loggoutButton.classList.toggle("show-loggout");
-      })
-      loggoutButton.addEventListener("click", () => {
-        firebase.auth().signOut();
-      });
-      document.getElementById("postados").innerHTML = "";
-      readPosts();
-      postDOM();
-    } else if (!user) {
-      container.innerHTML = "";
-      container.appendChild(routes.home);
-      const emailInput = document.querySelector("#email-input");
-      const passwordInput = document.querySelector("#password-input");
-      const loginButton = document.querySelector("#submit-btn");
-      loginButton.addEventListener("click", () => {
-        firebase
-          .auth()
-          .signInWithEmailAndPassword(emailInput.value, passwordInput.value)
-          .catch((error) => {
-            alert(error.message);
-          });
-      })
-      const registerButton = document.querySelector("#register");
-      registerButton.addEventListener("click", () => {
-        container.innerHTML = "";
-        container.appendChild(routes.register);
-        registerDOM();
-      });
+      loggoutMenuEvent(container);
+      pagePost();
 
-      const googleAuth = document.querySelector("#google");
-
-      googleAuth.addEventListener("click",googleLogin);
+    } else {
+      loginEvent(container);
+      registerEvent(container)
+      loginEventGoogle();
     }
   });
 }
@@ -51,7 +28,3 @@ function init() {
 window.addEventListener("load", () => {
   init();
 });
-
-
-
-
