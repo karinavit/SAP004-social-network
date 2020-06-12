f8d845fd41e8aaebbf66e67b5c5b8cbced3d73c1
jessicamelise <jessica.melise@hotmail.com>
2020-06-12 17:53:52 -0300

    implementando css versão web pagina de login e ajustando mobile

--- public/pages/login/login.js
+++ public/pages/login/login.js
@@ -1,16 +1,18 @@
 export const formLogin = () => {
   const container = document.createElement("div");
   container.classList.add("display-column");
+  container.classList.add("display-web");
 
   container.innerHTML = `
-      <img class="img" src="img/logo.png" alt="logo">
-      <h1 class="style-title">Social Trekkers</h1>
+    <img class="img img-web" src="img/logo.png" alt="logo">
+    <div class="display-column login-style-web">
+      <h1 class="style-title title-web">Social Trekkers</h1>
       <input class="style-input" id='email-input' type='email' placeholder="Digite seu e-mail...">
       <input class="style-input" id='password-input' type='password' placeholder="Digite sua senha...">
       <a class="width-button-login" href="/#posts">
-      <button class="button-login" id='submit-btn' >Logar</button>
+      <button class="button-login login-btn-web" id='submit-btn' >Logar</button>
       </a>
-      <p class="letter-color"> Esqueceu a senha,
+      <p class="letter-color margin-web"> Esqueceu a senha,
         <a class="link-register" href="">Clique aqui</a>
       </p>
       <p class="letter-color logo-google" id="google">Entre com 
@@ -18,7 +20,8 @@ export const formLogin = () => {
       </p>
       <p class="letter-color">Caso não possua conta ainda, 
         <a class="link-register" href="/#register" id="register">Registre-se</a>
-      </p>
+      </p>      
+    </div>
   `;
 
   return container;
