559dc62ba572bd09fc44ab8fd97b68d2c48863ba
Caroline Pinheiro <ommasshi@gmail.com>
2020-06-13 12:31:02 -0300

    Refatoração da função de login com google e facebook para apenas uma

--- public/data.js
+++ public/data.js
@@ -69,30 +69,8 @@ export function readPosts(func) {
   });
 }
 
-export function facebookLogin () {
-
-  var provider = new firebase.auth.FacebookAuthProvider();
-  firebase.auth().signInWithPopup(provider).then(function(result) {
-    // This gives you a Facebook Access Token. You can use it to access the Facebook API.
-    var token = result.credential.accessToken;
-    // The signed-in user info.
-    var user = result.user;
-    // ...
-  }).catch(function(error) {
-    // Handle Errors here.
-    var errorCode = error.code;
-    var errorMessage = error.message;
-    // The email of the user's account used.
-    var email = error.email;
-    // The firebase.auth.AuthCredential type that was used.
-    var credential = error.credential;
-    // ...
-  });
-}
-
-export function googleLogin() {
+export function googleAndFacebookLogin(provider) {
   // const userCollection = firebase.firestore().collection("users-info");
-  let provider = new firebase.auth.GoogleAuthProvider();
   provider.setCustomParameters({ prompt: 'select_account' });
   firebase
     .auth()
