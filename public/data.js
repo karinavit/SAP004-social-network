24537eb4fd52e4bf4ff83903448b2093100b1ec6
Caroline Pinheiro <ommasshi@gmail.com>
2020-06-12 23:13:27 -0300

    Adição da função de login com o facebook no firebase

--- public/data.js
+++ public/data.js
@@ -69,6 +69,27 @@ export function readPosts(func) {
   });
 }
 
+export function facebookLogin () {
+
+  var provider = new firebase.auth.FacebookAuthProvider();
+  firebase.auth().signInWithPopup(provider).then(function(result) {
+    // This gives you a Facebook Access Token. You can use it to access the Facebook API.
+    var token = result.credential.accessToken;
+    // The signed-in user info.
+    var user = result.user;
+    // ...
+  }).catch(function(error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    // The email of the user's account used.
+    var email = error.email;
+    // The firebase.auth.AuthCredential type that was used.
+    var credential = error.credential;
+    // ...
+  });
+}
+
 export function googleLogin() {
   // const userCollection = firebase.firestore().collection("users-info");
   let provider = new firebase.auth.GoogleAuthProvider();
