a4bba1d34a506209b6b269559068093d489f8a88
jessicamelise <jessica.melise@hotmail.com>
2020-06-15 00:06:05 -0300

    criando arquivos para separar a criação e manipulação do menu das páginas

---
+++ public/pages/posts/menufixed.js
@@ -0,0 +1,18 @@
+import { menuPost } from "./mainmenufixed.js";
+
+export const menuFixed = (pageRoot) => {
+  const container = document.createElement('div');
+  container.classList.add("display-column");
+  container.innerHTML = `
+    <div class="nav-posts">
+      <img id="bar-menu" class="bar-menu" src="img/bars-solid.svg" alt="bar">
+      <h1 class="logo-name-posts-nav">Social Trekkers</h1>
+      <img class="menu-posts" src="img/logo.png" alt="logo">
+    </div>
+    <a id="loggout" class="loggout" href="#">
+      <div>Sair</div>
+    </a>
+  `;
+  pageRoot.prepend(container);
+  menuPost.loggoutMenuEvent();
+}  
