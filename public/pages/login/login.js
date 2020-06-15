0f33b1b7d65bfb757ae29ec3608aa4320374c0a9
jessicamelise <jessica.melise@hotmail.com>
2020-06-14 22:53:14 -0300

    ajustando spa da página de login

--- public/pages/login/login.js
+++ public/pages/login/login.js
@@ -1,4 +1,6 @@
-export const formLogin = () => {
+import { initFunc } from "./mainlogin.js";
+
+export const formLogin = (root) => {
   const container = document.createElement("div");
   container.classList.add("display-column");
   container.classList.add("display-web");
@@ -9,9 +11,7 @@ export const formLogin = () => {
       <h1 class="style-title title-web">Social Trekkers</h1>
       <input class="style-input" id='email-input' type='email' placeholder="Digite seu e-mail...">
       <input class="style-input" id='password-input' type='password' placeholder="Digite sua senha...">
-      <a class="width-button-login" href="/#posts">
-      <button class="button-login login-btn-web" id='submit-btn' >Logar</button>
-      </a>
+      <button class="button-login login-btn-web width-button-login" id='submit-btn'>Logar</button>
       <p class="letter-color margin-web"> Esqueceu a senha,
         <a class="link-register" href="">Clique aqui</a>
       </p>
@@ -19,10 +19,14 @@ export const formLogin = () => {
         <img class="img-g" id="google" src="../../img/google.png" alt="logo-google">
         <img class="img-f"  id="facebook" src="../../img/facebook.png" alt="logo-facebook">
       <p class="letter-color">Caso não possua conta ainda, 
-        <a class="link-register" href="/#register" id="register">Registre-se</a>
+        <a class="link-register" href="#register" id="register">Registre-se</a>
       </p>      
     </div>
   `;
-
-  return container;
+  root.innerHTML = "";
+  root.appendChild(container);
+  initFunc.loginEvent();
+  initFunc.registerEvent();
+  initFunc.loginEventGoogle();
+  initFunc.loginEventFacebook();
 };
