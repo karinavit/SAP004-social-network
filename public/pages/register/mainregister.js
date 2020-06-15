ee3ef27b4cbde7dc44528806929050a24b07c1e4
jessicamelise <jessica.melise@hotmail.com>
2020-06-14 22:52:21 -0300

    criando arquivo main na pasta register para manipulação do dom da página de registro

---
+++ public/pages/register/mainregister.js
@@ -0,0 +1,20 @@
+import firebaseActions from "../../data.js";
+
+export function registerDOM() {
+  const emailRegisterInput = document.querySelector("#email-input-register");
+  const nameRegisterInput = document.querySelector("#name-input-register");
+  const birthdayRegisterInput = document.querySelector("#date-input-register");
+  const passwordRegisterInput = document.querySelector("#password-input-register");
+  const singInRegisterButton = document.querySelector("#sign-in-register-button");
+  const backButton = document.querySelector("#back-button");
+
+  backButton.addEventListener("click", () => {
+    window.location = "#";
+  });
+
+  singInRegisterButton.addEventListener("click", () => {
+    firebaseActions.register(emailRegisterInput.value, passwordRegisterInput.value, 
+      nameRegisterInput.value, birthdayRegisterInput.value);
+  });
+}
+
