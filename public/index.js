bf62f847d56cc6782b97ce4ae0c7d18bd0c8c061
jessicamelise <jessica.melise@hotmail.com>
2020-06-16 23:11:48 -0300

    refatoração das funções de spa

--- public/index.js
+++ public/index.js
@@ -1,33 +1,22 @@
-import { formLogin } from './pages/login/login.js';
-import { signIn } from './pages/posts/postsTemplate.js';
-import { register } from './pages/register/register.js';
-import { profilePage } from './pages/profile/profile.js'
+import routes from './routes.js';
 import { firebaseActions } from './data.js';
 
 const root = document.querySelector('#root');
 
 function hashs() {
   root.innerHTML = '';
-  switch (window.location.hash) {
-    case '':
-      formLogin(root);
-      break;
-    case '#register':
-      register(root);
-      break;
-    case '#posts':
-      setTimeout(() => {
-        signIn(root, firebaseActions.takeNameData());
-      }, 1000);
-      break;
-    case '#profile':
-      setTimeout(() => {
-      profilePage(root, firebaseActions.takeNameData());
+  const hashPage = verifyHash(window.location.hash);
+  if (hashPage === "home" || hashPage === "register") {
+    routes[hashPage](root);
+  } else {
+    setTimeout(() => {
+      routes[hashPage](root, firebaseActions.takeNameData());
     }, 1000);
-      break;
-    default:
-      formLogin(root);
-  };
+  }
+}
+
+function verifyHash(hash) {
+  return hash === '' ? "home" : hash.replace("#", '');
 }
 
 const changePages = () => {
@@ -38,16 +27,14 @@ const changePages = () => {
 
 function verifyUser() {
   const user = firebase.auth().currentUser;
-  const location = window.location.hash;
-  console.log(user);
   if (user) {
-    if (location !== "#profile") {
+    if (window.location.hash !== "#profile") {
     window.location = "#posts";
-    }     
-  } else {
-    if (location !== "#register") {
-    window.location = "#";
     } 
+  } else {
+    if (window.location.hash !== "#register") {
+    window.location = "#";
+    }
   }
   hashs();
 }
@@ -64,6 +51,6 @@ function init() {
 
 window.addEventListener('load', () => {
   init();
-  hashs()
+  hashs();
   changePages();
 });
