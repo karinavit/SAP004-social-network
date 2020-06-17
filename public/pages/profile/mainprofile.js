45f24230f47be1e0b677d21c32cfd295e91a2eb3
jessicamelise <jessica.melise@hotmail.com>
2020-06-16 23:47:19 -0300

    separação de funções entre arquivos da pasta de profile conforme separação da pasta posts

--- public/pages/profile/mainprofile.js
+++ public/pages/profile/mainprofile.js
@@ -1,7 +1,13 @@
+import { createElementProfilePost } from './profileAndComments.js'
+
 export function backPosts() {
   const buttonBack = document.querySelector('#button-back-posts');
   buttonBack.addEventListener('click', () => {
     window.location = '#posts';
   });
+}
+
+export function readPostsProfileDOM(post, element) {
+  element.querySelector('#profile-posts').prepend(createElementProfilePost(post));
 }
 
--- public/routes.js
+++ public/routes.js
@@ -1,13 +1,13 @@
 import { formLogin } from './pages/login/login.js';
 import { signIn } from './pages/posts/postsTemplate.js';
 import { register } from './pages/register/register.js';
-import { profilePage } from './pages/profile/profile.js';
+import { profilePage } from './pages/profile/profileTemplate.js';
 
 const routes = {
   home: formLogin,
   register,
   posts: signIn,
   profile: profilePage,
-}
+};
 
 export default routes;
