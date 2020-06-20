f358e23ac20adc75818ffc2f323c4eae91cfab5f
Caroline Pinheiro <ommasshi@gmail.com>
2020-06-20 17:47:22 -0300

    mainRegister corrigido com eslint e o update de imagens retirado

--- public/pages/register/mainregister.js
+++ public/pages/register/mainregister.js
@@ -6,23 +6,10 @@ export function registerDOM() {
   form.backButton.addEventListener('click', () => {
     window.location = '#';
   });
-  let imgProfile;
-  form.profileImage.addEventListener("change", event => {
-      let arquivo = event.target.files[0];
-      var ref = firebase.storage().ref('arquivo')
-      ref.child('arquivo' + arquivo.name).put(arquivo).then(() => {
-        ref.child('arquivo' + arquivo.name).getDownloadURL().then(url => {
-          imgProfile = url;
-          console.log('postei')
-        })
-    })
-      
-  });
-  console.log(imgProfile)
 
   form.signRegister.addEventListener('click', (event) => {
     event.preventDefault();
     firebaseActions.register(form.emailRegister.value, form.passwordRegister.value,
-      form.nameRegister.value, form.dateRegister.value, imgProfile);
+      form.nameRegister.value, form.dateRegister.value);
   });
 }
