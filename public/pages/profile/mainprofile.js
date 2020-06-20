2c04ae9b69676c8061603264bcfa9b4b59c27180
karinavit <ka.vitangelo@gmail.com>
2020-06-20 15:59:43 -0300

    criada a função editProfile e inserido o popup

--- public/pages/profile/mainprofile.js
+++ public/pages/profile/mainprofile.js
@@ -10,3 +10,24 @@ export function backPosts() {
 export function readPostsProfileDOM(post, element) {
   element.querySelector('#profile-posts').prepend(createElementProfilePost(post));
 }
+
+export function editProfile(){
+  const buttonEdit = document.getElementById('edit-profile')
+  buttonEdit.addEventListener('click', () => {
+    const popup = document.getElementById('popup');
+    popup.innerHTML = '';
+    popup.classList.remove('popup-none');
+    popup.classList.add('popup');
+    const editAreaPopUp = `<label>Nome</label> 
+    <input type='text'>
+    <label>Data de Nascimento</label>
+    <input type='date'>
+    <label>Foto</label>
+    <input type='file'>
+    <label>Patente</label>
+    <input type='text'>
+    `;
+    popup.innerHTML = editAreaPopUp;
+  })
+
+}
