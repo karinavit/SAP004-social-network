8c6edebb1ebce5348b05ef49ed3379156388b931
karinavit <ka.vitangelo@gmail.com>
2020-06-20 16:10:39 -0300

    Inserido paramêtro posts

--- public/pages/profile/mainprofile.js
+++ public/pages/profile/mainprofile.js
@@ -11,7 +11,7 @@ export function readPostsProfileDOM(post, element) {
   element.querySelector('#profile-posts').prepend(createElementProfilePost(post));
 }
 
-export function editProfile(){
+export function editProfile(posts){
   const buttonEdit = document.getElementById('edit-profile')
   buttonEdit.addEventListener('click', () => {
     const popup = document.getElementById('popup');
@@ -19,15 +19,17 @@ export function editProfile(){
     popup.classList.remove('popup-none');
     popup.classList.add('popup');
     const editAreaPopUp = `<label>Nome</label> 
-    <input type='text'>
+    <input type='text' value='${posts.data().nameUser}'>
     <label>Data de Nascimento</label>
-    <input type='date'>
+    <input type='date' value='${posts.data().birthdayUser}'>
     <label>Foto</label>
     <input type='file'>
     <label>Patente</label>
     <input type='text'>
     `;
     popup.innerHTML = editAreaPopUp;
+
+
   })
 
 }
