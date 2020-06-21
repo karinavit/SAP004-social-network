cf9f17e68ca502e950eb7580e43688ba226f7a5b
karinavit <ka.vitangelo@gmail.com>
2020-06-21 17:59:37 -0300

    Inserido o confirmPassowrdReset no handleErrors

--- public/handleErrors.js
+++ public/handleErrors.js
@@ -10,4 +10,25 @@ export const errLogin = [{
   code: 'auth/wrong-password',
   message: 'Senha incorreta, favor tentar novamente ou solicitar a recuperação de senha abaixo',
 },
+
+
 ];
+
+export const confirmPasswordReset = [{
+  code: 'auth/expired-action-code',
+  message: 'Lançado se o código de redefinição de senha expirou.',
+},
+{
+  code: 'auth/invalid-action-code',
+  message: 'Lançado se o código de redefinição de senha for inválido. Isso pode acontecer se o código estiver malformado ou já tiver sido usado.',
+},
+{
+  code: 'auth/user-disabled',
+  message: 'Lançado se o usuário correspondente ao código de redefinição de senha fornecido tiver sido desativado.',
+},
+{ code: 'auth/user-not-found',
+  message: 'Lançado se não houver usuário correspondente ao código de redefinição de senha. Isso pode ter acontecido se o usuário foi excluído entre quando o código foi emitido e quando esse método foi chamado.',
+},
+]
+
+
