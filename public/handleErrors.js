42aec1f30b1c3512f0090bdefb1fc3d397bb09ca
karinavit <ka.vitangelo@gmail.com>
2020-06-21 18:03:54 -0300

    Correção da ConfirmPassorwReset

--- public/handleErrors.js
+++ public/handleErrors.js
@@ -16,19 +16,24 @@ export const errLogin = [{
 
 export const confirmPasswordReset = [{
   code: 'auth/expired-action-code',
-  message: 'Lançado se o código de redefinição de senha expirou.',
+  message: 'O código de redefinição de senha expirou.',
 },
 {
   code: 'auth/invalid-action-code',
-  message: 'Lançado se o código de redefinição de senha for inválido. Isso pode acontecer se o código estiver malformado ou já tiver sido usado.',
+  message: 'O código de redefinição de senha é inválido. Isso pode acontecer se o código estiver malformado ou já tiver sido usado.',
 },
 {
   code: 'auth/user-disabled',
-  message: 'Lançado se o usuário correspondente ao código de redefinição de senha fornecido tiver sido desativado.',
+  message: 'O usuário correspondente ao código de redefinição de senha fornecido tiver sido desativado.',
 },
 { code: 'auth/user-not-found',
-  message: 'Lançado se não houver usuário correspondente ao código de redefinição de senha. Isso pode ter acontecido se o usuário foi excluído entre quando o código foi emitido e quando esse método foi chamado.',
+  message: 'Não há usuário correspondente ao código de redefinição de senha. Isso pode ter acontecido se o usuário foi excluído entre quando o código foi emitido e quando esse método foi chamado.',
+},
+{ code: 'auth/weak-password',
+  message:'A nova senha não é forte o suficiente.'
 },
 ]
+
+
 
 
