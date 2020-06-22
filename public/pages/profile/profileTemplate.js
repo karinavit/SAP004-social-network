211b38d73393c5bfdea6f266020456c3f42f86e0
Caroline Pinheiro <ommasshi@gmail.com>
2020-06-22 11:32:39 -0300

    Criação da div que recebe a alteração da imagem de perfil, para que ela se atualize dinamicamente com o usuário

--- public/pages/profile/profileTemplate.js
+++ public/pages/profile/profileTemplate.js
@@ -2,14 +2,14 @@ import { menuFixed } from '../posts/menu/menufixed.js';
 import { readPostsProfileDOM, editProfile } from './mainprofile.js';
 import { firebaseActions, profileUpdate } from '../../data.js';
 
-export const profilePage = (root, name = '', photo) => {
+export const profilePage = (root, name = '') => {
   const container = document.createElement('div');
   container.classList.add('display-column');
   container.innerHTML = `
     <div class="display-mobile display-web-row">
       <div class="margin-top-user profile-mobile display-web-user profile-web">
-        <img class="img-user img-user-profile" src=${photo} alt="spock">
-        <div class="name-user">
+      <div class="img-user img-user-web" id='photo-area' alt="spock"></div>
+      <div class="name-user">
           <h1>Bem vindo <span id='true-name'> ${name}</span></h1>
           <br>
           <p>Number One - Classic</p>
