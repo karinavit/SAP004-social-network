e8f5a34c8fb89cca899ae9c31a2a3e744947a0ff
Caroline Pinheiro <ommasshi@gmail.com>
2020-06-23 14:52:15 -0300

    Incremento e decremento dos valores acontece diretamente no parâmetro, reduzindo algumas linhas, além da retirada da entrada likes no update que não é mais considerada pelo código

--- public/data.js
+++ public/data.js
@@ -177,25 +177,21 @@ export function oneLikePerUserComments(document) {
     .then((posts) => {
       if (posts.data().wholiked.includes(firebase.auth().currentUser.uid)) {
         let likeComment = posts.data().wholiked.length;
-        likeComment -= 1;
-        document.func(likeComment, document.element);
+        document.func(likeComment--, document.element);
         firebaseActions.editOrLikeComments({
           docId: document.docId,
           update: {
             wholiked: firebase.firestore.FieldValue.arrayRemove(firebase.auth().currentUser.uid),
-            likes: likeComment,
           },
           postId: document.postId
         });
       } else {
         let likeComment = posts.data().wholiked.length;
-        likeComment += 1;
-        document.func(likeComment, document.element);
+        document.func(likeComment++, document.element);
         firebaseActions.editOrLikeComments({
           docId: document.docId,
           update: {
             wholiked: firebase.firestore.FieldValue.arrayUnion(firebase.auth().currentUser.uid),
-            likes: likeComment,
           },
           postId: document.postId
         });
